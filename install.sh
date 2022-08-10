54d64d4807d1fad2d1fc5f1d0d5acec2a3f3787a
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2022-08-10 19:06:22 +0300

    Add tools build-vz7 and build-vz9 to build Virtuozzo kernel in docker
    Signed-off-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- install.sh
+++ install.sh
@@ -11,5 +11,8 @@ cp kinst ~/bin
 cp kmake ~/bin
 cp modstrip ~/bin
 cp nfs-export-dir.sh ~/bin
+cp build-vz7 ~/bin
+cp rh-conf ~/bin
+cp build-vz9 ~/bin
 
 echo "export PATH=\"$HOME/bin:$PATH\"" > ~/.bashrc
